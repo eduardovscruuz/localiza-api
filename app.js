00fc5af6a07edf6d98a4b56c1c119493f9771542
eduardovscruuz <eduardovscruuz@gmail.com>
2023-06-02 23:29:55 -0300

    carro rota em app.js

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
-const database = require('./configs/database')
 const express = require('express')
+const database = require('./configs/database')
 
-database.mongoose.connect(database.url, 
-        {useNewUrlParser: true, 
-        useUnifiedTopology: true
-        }).then(()=>{
-            console.log('Conexão estabelecida com o banco de dados')
-        }).catch(error => {
-            console.log('Falha ao estabelecer conexão com o banco de dados', error);
-            process.exit()
-        });
+// conexão com o banco de dados
+database.mongoose
+  .connect(database.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log("Conexão estabelecida com o banco de dados.");
+  }).catch(err => {
+    console.log("Não conseguiu conectar com o banco de dados.", err);
+    process.exit();
+  });
 
 // criando uma aplicação express
-const app = express()
+const app = express();
 
-//definindo que vamos enviar e receber JSON na requisição
+// definindo que vamos enviar e receber JSON na requisição
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-const PORT = 3000
-const HOST = '0.0.0.0'
+// definindo nossas rotas
+// var promocaoRoute = require('./routes/promocaoRoute');
+// app.use('/', promocaoRoute);
 
-app.listen(PORT,HOST, () => {
-    console.log(`Servidor executando na porta ${PORT}`)
-})
+// var clienteRoute = require('./routes/clienteRoute');
+// app.use('/', clienteRoute);
+
+// var funcionarioRoute = require('./routes/funcionarioRoute');
+// app.use('/', funcionarioRoute);
+
+var carroRouter = require('./routes/carroRouter');
+app.use('/', carroRouter);
+
+// var esportivoRouter = require('./routes/esportivoRouter');
+// app.use('/', esportivoRouter);
+
+// var utilitarioRouter = require('./routes/utilitarioRouter');
+// app.use('/', utilitarioRouter);
+
+// var reservaRoute = require('./routes/reservaRoute');
+// app.use('/', reservaRoute);
+
+// definindo nossa porta e host
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor executando na porta ${PORT}.`);
+});
